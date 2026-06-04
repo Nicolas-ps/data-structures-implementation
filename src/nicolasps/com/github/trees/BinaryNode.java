7d10a3fc7cb8b82cebd8e3af7ee5ee29fc087158
nicolas237 <nicolas@focomultimidia.com>
2026-06-03 23:34:16 -0300

    rename TripleNode to BinaryNode and update references in the class

---
+++ src/nicolasps/com/github/trees/BinaryNode.java
@@ -0,0 +1,44 @@
+package nicolasps.com.github.trees;
+
+public class BinaryNode<T> {
+    private T value;
+    private BinaryNode<T> left;
+    private BinaryNode<T> right;
+    private BinaryNode<T> parent;
+
+    public BinaryNode(T data) {
+        this.value = data;
+    }
+
+    public BinaryNode<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(BinaryNode<T> left) {
+        this.left = left;
+    }
+
+    public BinaryNode<T> getRight() {
+        return right;
+    }
+
+    public void setRight(BinaryNode<T> right) {
+        this.right = right;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public BinaryNode<T> getParent() {
+        return parent;
+    }
+
+    public void setParent(BinaryNode<T> parent) {
+        this.parent = parent;
+    }
+}
